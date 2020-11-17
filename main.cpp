dea5196716afca97d48bb03d04af8a19f33eb06e
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-11-17 14:55:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ void dijkstra(vector<vector<int>> graph, int size, int start) {
     }
     cout << "Node   Min Distance" << endl;
     for(int i = 0; i<size; i++) {
-        cout << i << "   " <<  distance[i];
+        cout << i << "   " <<  distance[i] << endl;
     }
 }
 
