3a05363a48184db9413d503b43be9a68823f63e1
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-11-17 14:49:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void dijkstra(vector<vector<int>> graph, int size, int start) {
     }
     for(int i = 0; i<size-1; i++) {
         int index;
-        int short = 9999;
+        
         for(int j = 0; j<size; j++) {
                if((distance[j]<=short) && (shortest[j]==0)) {
                     short = distance[j];
