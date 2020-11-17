24010c07be845bcf431d1ac1170ac352d611a87d
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-11-17 13:49:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define INF 9999
 int gsize = 0;
 
-void dijkstra(int graph[6][6], int size, int start) {
+void dijkstra(int graph[gsize][gsize], int size, int start) {
     int distance[size];
     int shortest[size];
     for(int i = 0; i<size; i++) {
@@ -45,7 +45,7 @@ void dijkstra(int graph[6][6], int size, int start) {
 int main() {
    int sizea = 6;
    gsize = 6;
-   int a[sizea][sizea] = { {0, 2, 5, 1, 0, 0},
+   int a[gsize][gsize] = { {0, 2, 5, 1, 0, 0},
                    {2, 0, 3, 2, 0, 0},
                    {5, 3, 0, 3, 1, 5},
                    {1, 2, 3, 0, 1, 0},
@@ -55,7 +55,7 @@ int main() {
   
   int sizeb = 7;
   gsize = 7;
-  int b[sizeb][sizeb] = { {0, 2, 4, 0, 0, 7, 0}, 
+  int b[gsize][gsize] = { {0, 2, 4, 0, 0, 7, 0}, 
                   {2, 0, 3, 3, 0, 0, 0},
                   {4, 3, 0, 4, 3, 8, 0},
                   {0, 3, 4, 0, 6, 0, 0},
@@ -63,7 +63,7 @@ int main() {
                   {7, 0, 8, 0, 6, 0, 12},
                   {0, 0, 0, 0, 8, 12, 0} };
  
-   //dijkstra(b, sizeb, 3);
+   dijkstra(b, sizeb, 3);
    
                   
 }
